6ff6c5ed871fd3e2e73832959c6fc1d85e828700
teocalvo2@gmail.com <teocalvo2@gmail.com>
2022-02-19 21:54:45 +0000

    pousando dados de detalhes de cada partida
    Para cada partida estamos pousando seus dados brutos em camada raw com formato json.
    
    Bem como realizando  controle de quais partidas ja foram adicionadas e quais não.

--- raw/get_pro_matches_details.py
+++ raw/get_pro_matches_details.py
@@ -1,8 +1,14 @@
 # Databricks notebook source
+# MAGIC %pip install tqdm
+
+# COMMAND ----------
+
 import json
 import requests
 from pyspark.sql.types import *
 from pyspark.sql import functions as F
+
+from tqdm import tqdm
 
 # COMMAND ----------
 
@@ -14,23 +20,42 @@ def get_data(match_id, **kwargs):
 def get_and_land_match_details(match_id):
     path = f"/mnt/datalake/raw/pro_matches_landing/{match_id}.json"
     data = get_data(match_id=match_id)
-    data_str = json.dumps(data)
-    try:
-        dbutils.fs.put(path, data_str)
-        ( spark.createDataFrame([{"match_id":match_id, "path":path}])
-               .write
-               .format("parquet")
-               .mode("append")
-               .save("/mnt/datalake/raw/pro_matches_proceeded") )
+    
+    if "match_id" in data:
+    
+        data_str = json.dumps(data)
+        try:
+            dbutils.fs.put(path, data_str)
+            ( spark.createDataFrame([{"match_id":match_id, "path":path}])
+                   .write
+                   .format("delta")
+                   .mode("append")
+                   .save("/mnt/datalake/raw/pro_matches_proceeded") )
 
-    except:
-        print("O arquivo já existe")
+        except:
+            print("O arquivo já existe")
+            
+def get_pro_matches_ids():
+    df_history = spark.read.format("delta").load("/mnt/datalake/bronze/pro_matches_history")
+    
+    if "pro_matches_proceeded" in [i.name.strip("/") for i in dbutils.fs.ls("/mnt/datalake/raw/")]
+        print("Verificando partidas coletadas")
+        
+        df_proceeded = ( spark.read
+                              .format("delta")
+                              .load("/mnt/datalake/raw/pro_matches_proceeded") )
+        
+        return ( df_history.join(df_proceeded, "match_id", "left")
+                           .filter("path is null")
+                           .select("match_id") )
+
+    else:
+        print("Histórico de processo não encontrado")
+        return df_history.select("match_id")
 
 # COMMAND ----------
 
-tables = [i.name.strip("/") for i in dbutils.fs.ls("/mnt/datalake/raw/")]
-tables
+match_ids = get_pro_matches_ids()
 
-# COMMAND ----------
-
-matches_ids = spark.read.format("delta").load("/mnt/datalake/bronze/pro_matches_history").select("match_id")
+for i in tqdm(match_ids.collect()):
+    get_and_land_match_details(i[0])
