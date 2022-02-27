24c248349028651535067d09e4308652e9ed13e5
teocalvo2@gmail.com <teocalvo2@gmail.com>
2022-02-27 02:58:06 +0000

    Stream para processar jsons

--- raw/process_pro_matches_details.py
+++ raw/process_pro_matches_details.py
@@ -1,69 +1,6 @@
 # Databricks notebook source
-# MAGIC %pip install tqdm
-
-# COMMAND ----------
-
-import requests
 from pyspark.sql.types import *
 from pyspark.sql import functions as F
-import json
-from tqdm import tqdm
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC 
-# MAGIC ### Tabela match
-
-# COMMAND ----------
-
-MATCH_SCHEMA = StructType( [StructField('match_id',LongType(), True),
-                            StructField('barracks_status_dire',LongType(), True),
-                            StructField('barracks_status_radiant',LongType(), True),
-                            StructField('cluster',LongType(), True),
-                            StructField('dire_score',LongType(), True),
-                            StructField('dire_team_id',LongType(), True),
-                            StructField('duration',LongType(), True),
-                            StructField('engine',LongType(), True),
-                            StructField('first_blood_time',LongType(), True),
-                            StructField('game_mode',LongType(), True),
-                            StructField('human_players',LongType(), True),
-                            StructField('leagueid',LongType(), True),
-                            StructField('lobby_type',LongType(), True),
-                            StructField('match_seq_num',LongType(), True),
-                            StructField('negative_votes',LongType(), True),
-                            StructField('positive_votes',LongType(), True),
-                            StructField('radiant_score',LongType(), True),
-                            StructField('radiant_team_id',LongType(), True),
-                            StructField('radiant_win',BooleanType(), True),
-                            StructField('start_time',LongType(), True),
-                            StructField('tower_status_dire',LongType(), True),
-                            StructField('tower_status_radiant',LongType(), True),
-                            StructField('version',LongType(), True),
-                            StructField('replay_salt',LongType(), True),
-                            StructField('series_id',LongType(), True),
-                            StructField('series_type',LongType(), True),
-                            StructField('league', StructType([
-                                 StructField('leagueid',LongType(),True),
-                                 StructField('ticket',StringType(),True),
-                                 StructField('banner',StringType(),True),
-                                 StructField('tier',StringType(),True),
-                                 StructField('name',StringType(),True)]),True),
-                            StructField('radiant_team', StructType([
-                                  StructField('team_id',LongType(),True),
-                                  StructField('name',StringType(),True),
-                                  StructField('tag',StringType(),True),
-                                  StructField('logo_url',StringType(),True)]),True),
-                            StructField('dire_team', StructType([
-                                  StructField('team_id',LongType(),True),
-                                  StructField('name',StringType(),True),
-                                  StructField('tag',StringType(),True),
-                                  StructField('logo_url',StringType(),True)]),True),
-                            StructField('patch',LongType(), True),
-                            StructField('region',LongType(), True),
-                            StructField('throw',LongType(), True),
-                            StructField('loss',LongType(), True),
-                            StructField('replay_url', StringType(), True),])
 
 # COMMAND ----------
 
@@ -168,73 +105,114 @@ MATCH_PLAYERS_SCHEMA = StructType([StructField("match_id", LongType(), True),
 
 # COMMAND ----------
 
-def get_data(match_id):
-    path = f"/dbfs/mnt/datalake/raw/pro_matches_landing/{match_id}.json"
-    return json.load(open(path,"r"))
-
-def save_data(df, tb_name):
-    ( df.coalesce(1)
-        .write
-        .mode("append")
-        .format("parquet")
-        .save(f"/mnt/datalake/raw/{tb_name}"))
-
-def save_match_df(data):
-    df = spark.createDataFrame([data], schema=MATCH_SCHEMA)
-    save_data(df, "tb_pro_matches")
-    
-def save_match_chat_df(data):
-      
-    if data["chat"] != None and len(data["chat"]) > 0:
-        df = spark.createDataFrame(data["chat"])
-        df = df.withColumn("match_id", F.lit(data["match_id"]) )
-        save_data(df, "tb_pro_match_chat")
-
-def save_match_picks_bans(data):
-    if data["picks_bans"] != None:
-        df = spark.createDataFrame(data["picks_bans"])
-        save_data(df, "tb_pro_match_picks_bans")
-
-def save_radiant_gold_adv_df(data):
-    if data["radiant_gold_adv"] != None:
-        data_new = [ {"value": i, "match_id":data["match_id"]} for i in data["radiant_gold_adv"] ]
-        df = spark.createDataFrame(data_new)
-        save_data(df, "tb_pro_match_radiant_gold_adv")
-
-def save_radiant_xp_adv_df(data):
-    if data["radiant_xp_adv"] != None:
-        data_new = [ {"value": i, "match_id":data["match_id"]} for i in data["radiant_xp_adv"] ]
-        df = spark.createDataFrame(data_new)
-        save_data(df, "tb_pro_match_radiant_xp_adv")
-
-def save_match_player_df(data):
-    df = spark.createDataFrame(data["players"], schema=MATCH_PLAYERS_SCHEMA)
-    save_data(df, "tb_pro_matches_players")
-
-def process_match(match_id):
-    
-    data = get_data(match_id)
-
-    save_match_chat_df(data)
-    save_match_picks_bans(data)
-    save_radiant_gold_adv_df(data)
-    save_radiant_xp_adv_df(data)
-    save_match_player_df(data)
-    save_match_df(data)
-    
-def get_ids():
-    df_proceeded = spark.read.format("delta").load("/mnt/datalake/raw/pro_matches_proceeded/")
-    
-    if "tb_pro_matches" in [i.name.strip("/") for i in dbutils.fs.ls("/mnt/datalake/raw/")]:
-        df_pro_matches = spark.read.parquet("/mnt/datalake/raw/tb_pro_matches/")
-
-        df_proceeded = (df_proceeded.join(df_pro_matches, "match_id", "left")
-                                    .filter("duration is null"))
-
-    return df_proceeded.select("match_id")
+# MAGIC %md
+# MAGIC 
+# MAGIC ### Tabela match
 
 # COMMAND ----------
 
-match_ids = get_ids()
-for i in tqdm(match_ids.collect()):
-    process_match(i[0])
+MATCH_SCHEMA = StructType( [StructField('match_id',LongType(), True),
+                            StructField('barracks_status_dire',LongType(), True),
+                            StructField('barracks_status_radiant',LongType(), True),
+                            StructField('cluster',LongType(), True),
+                            StructField('dire_score',LongType(), True),
+                            StructField('dire_team_id',LongType(), True),
+                            StructField('duration',LongType(), True),
+                            StructField('engine',LongType(), True),
+                            StructField('first_blood_time',LongType(), True),
+                            StructField('game_mode',LongType(), True),
+                            StructField('human_players',LongType(), True),
+                            StructField('leagueid',LongType(), True),
+                            StructField('lobby_type',LongType(), True),
+                            StructField('match_seq_num',LongType(), True),
+                            StructField('negative_votes',LongType(), True),
+                            StructField('positive_votes',LongType(), True),
+                            StructField('radiant_score',LongType(), True),
+                            StructField('radiant_team_id',LongType(), True),
+                            StructField('radiant_win',BooleanType(), True),
+                            StructField('start_time',LongType(), True),
+                            StructField('tower_status_dire',LongType(), True),
+                            StructField('tower_status_radiant',LongType(), True),
+                            StructField('version',LongType(), True),
+                            StructField('replay_salt',LongType(), True),
+                            StructField('series_id',LongType(), True),
+                            StructField('series_type',LongType(), True),
+                            StructField('league', StructType([
+                                 StructField('leagueid',LongType(),True),
+                                 StructField('ticket',StringType(),True),
+                                 StructField('banner',StringType(),True),
+                                 StructField('tier',StringType(),True),
+                                 StructField('name',StringType(),True)]),True),
+                            StructField('radiant_team', StructType([
+                                  StructField('team_id',LongType(),True),
+                                  StructField('name',StringType(),True),
+                                  StructField('tag',StringType(),True),
+                                  StructField('logo_url',StringType(),True)]),True),
+                            StructField('dire_team', StructType([
+                                  StructField('team_id',LongType(),True),
+                                  StructField('name',StringType(),True),
+                                  StructField('tag',StringType(),True),
+                                  StructField('logo_url',StringType(),True)]),True),
+                            StructField('patch',LongType(), True),
+                            StructField('region',LongType(), True),
+                            StructField('throw',LongType(), True),
+                            StructField('loss',LongType(), True),
+                            StructField('replay_url', StringType(), True),
+                            StructField('players', ArrayType(MATCH_PLAYERS_SCHEMA), True),
+                           ])
+
+# COMMAND ----------
+
+def get_data():
+    path = f"/mnt/datalake/raw/pro_matches_landing"
+    return spark.read.schema(MATCH_SCHEMA).json(path)
+
+def save_data(df, tb_name, mode):
+    (df.coalesce(3)
+       .write
+       .mode(mode)
+       .format("parquet")
+       .save(f"/mnt/datalake/raw/{tb_name}"))
+
+def etl_match_df(df, drop=[]):
+    df_match = df.drop(*drop)
+    return df_match   
+
+def etl_match_player_df(df):
+    df_match_players = (df.withColumn("players", F.explode("players") )
+                          .select("players.*") )
+    return df_match_players
+
+def upsert(df, batchId):
+    df_match = etl_match_df(df, ["players"])
+    df_match_player = etl_match_player_df(df)
+    
+    save_data(df_match_player,"tb_pro_matches_players", "append")
+    save_data(df_match,"tb_pro_matches", "append")
+
+# COMMAND ----------
+
+def upsert(df, batchId):
+    df_match = etl_match_df(df, ["players"])
+    df_match_player = etl_match_player_df(df)
+    
+    save_data(df_match_player,"tb_pro_matches_players", "append")
+    save_data(df_match,"tb_pro_matches", "append")
+
+df_stream = ( spark.readStream
+                   .format('cloudFiles')
+                   .option('cloudFiles.format', 'json')
+                   .schema(MATCH_SCHEMA)
+                   .load("/mnt/datalake/raw/pro_matches_landing") )
+
+stream = (df_stream.writeStream
+                   .foreachBatch(upsert)
+                   .option('checkpointLocation', f"/mnt/datalake/raw/json_to_parquet_checkpoint")
+                   .outputMode("update")
+                   .start() 
+         )
+
+# COMMAND ----------
+
+stream.processAllAvailable()
+stream.stop()
